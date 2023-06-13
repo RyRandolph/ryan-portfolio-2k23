d19659ed9e8f69115acbb300250624e831cf233b
Ryan R <Hythlodeous@yahoo.com>
2023-06-13 13:58:51 -0500

    feat(shimmy): shimmy text for skills section

--- src/app/components/parallaxSkillsContent.tsx
+++ src/app/components/parallaxSkillsContent.tsx
@@ -22,19 +22,23 @@ const ParralaxSkillsContent: React.FC = () => {
     "GraphQL", // 16
     "Typescript",
     "CI / CD",
+    "Microservices Architecture "
   ];
 
   const renderSkillItems = () => {
     let skillJSX = [];
     for (let i = 0; i < skillsList.length; i++) {
-      skillJSX[i] = (<span className={`${classes['font-cursive']} text-3xl text-orange-300 flex items-center justify-center h-full`}>{skillsList[i]}</span>) 
+      const randomNumber = Math.floor(Math.random() * 8) + 1;
+      console.log(randomNumber);
+      const shimmyClass = classes[`shimmy-${randomNumber}`];
+      skillJSX[i] = (<span className={`${classes['font-cursive']} ${shimmyClass} text-3xl text-orange-300 flex items-center justify-center h-full`}>{skillsList[i]}</span>) 
     }
     return skillJSX;
   }
 
   return (
     <div className="flex flex-col h-screen w-screen">
-      <div className="flex-1 grid grid-cols-6 grid-rows-3 gap-4 p-4">
+        <div className={`flex-1 grid grid-cols-6 grid-rows-3 gap-4 p-4`}>
         {renderSkillItems()}
       </div>
     </div>
